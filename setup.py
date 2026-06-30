c98f295d3b260bee3da539e6d64bc4e5f6ec2f9c
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-06-30 13:02:42 +0800

    alvistack/3.0
        git clean -xdf
        tar zcvf ../python-bracex_3.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-bracex.spec ../python-bracex_3.0-1.spec
        cp ../python*-bracex*3.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/facelessuser-bracex-3.0/
        rm -rf ../python*-bracex*3.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='bracex',
+    version='3.0',
+    description='Bash style brace expander.',
+    author_email='Isaac Muse <Isaac.Muse@gmail.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+    ],
+    packages=[
+        'bracex',
+    ],
+)
